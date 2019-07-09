bf51089f01f32a9a021bb9640a021879e6c7073c
Andres Ramirez Morales <andres.ramirez.morales@cern.ch>
2019-07-09 19:20:22 +0100

    mensaje de commit

--- CodigoDePractica/CodigoPython/function.py
+++ CodigoDePractica/CodigoPython/function.py
@@ -1,31 +1,32 @@
-#Una funcion es un bloque de codigo que solo corre cuando se  le llama. En python no usamos corchetes, usamos intentacion con tabs o espacios
+#Una funcion es un bloque de codigo que solo corre cuando se  le llama. En python no usamos corchetes, usamos indentacion con tabs o espacios
 
 #Crear a function, esta funcion no regresa nada solo imprime
 def decirHola(nombre=''):
-    print(f'Hola {nombre}')
+    print('Hola {0}'.format(nombre))
 
 decirHola('Andres')
 decirHola()
 
 #Funcion que me regresa un valor
-
 def hacerSuma(num1, num2):
     total = num1 + num2
-    return total
+    return float(total)
 
 hacerSuma(2,3)
-print(hacerSuma(2,3))
+print(hacerSuma(2,3),type(hacerSuma(2,3)))
 
-total = hacerSuma(2,3)
-print(total,"**lo mismo pero mas cool**")
+x = 2
+y = 2
+total = hacerSuma(x,y)
+print(total,type(total),"**lo mismo pero mas cool**")
 
 
-#FUNCION lambda
-# Una funcion lambda es una funcion anonima
-# Una funcion lambda puede tomar cualquier numero de argumentos, pero tener solo una expresion
+# #FUNCION lambda
+# # Una funcion lambda es una funcion anonima
+# # Una funcion lambda puede tomar cualquier numero de argumentos, pero tener solo una expresion
 
-print("--------")
-hacerSuma2 = lambda num1, num2 : num1 + num2
+# print("--------")
+# hacerSuma2 = lambda num1, num2 : num1 + num2
 
-print(hacerSuma2(10,3))
+# print(hacerSuma2(10,3))
 
--- CodigoDePractica/CodigoPython/loops.py
+++ CodigoDePractica/CodigoPython/loops.py
@@ -15,25 +15,27 @@ for person in people:
     print('Current Person: {0}'.format(person))
 
 #Continue
-# print('****Continue loop****')
-# for person in people:
-#     if person == 'Benito':
-#         continue
-#     print(f'Current Person: {person}')
+print('****Continue loop****')
+for person in people:
+    if person == 'Benito':
+       continue
+    print('Current Person: {0}'.format(person))
+    print('Esta orden tiene que estar indentada')
 
 
-# print('****Range loop****')
-# #range
-# for i in range(len(people)):
-#     print(people[i])
+print('****Range loop****')
+#range
+for i in range(len(people)):
+    print(people[i])
 
-# for i in range(0,11):
-#     print(f'Number: {i}')
+for i in range(0,len(people)):
+    print('Number: {0}'.format(i))
+    print('Orden extra para ver la importancia de indentacion')
 
     
-# #While loops: hasta que se cumpla una condicion
+#While loops: hasta que se cumpla una condicion
 
-# count = 0
-# while count <= 10:
-#     print(f'Count: {count}')
-#     count+=1
+count = 0
+while count <= 10:
+    print('Count: {0}'.format(count))
+    count+=1
